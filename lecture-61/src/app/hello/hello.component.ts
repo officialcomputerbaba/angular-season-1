1a0d376f47c615dfb26de6cdf0818ed58da84606
Ajit Rana <ajit.thakurs210@gmail.com>
2021-09-24 06:22:37 +0530

    lecture 61 example 7

--- lecture-61/src/app/hello/hello.component.ts
+++ lecture-61/src/app/hello/hello.component.ts
@@ -9,8 +9,15 @@ export class HelloComponent implements AfterViewInit {
   isLoading = true;
 
   ngAfterViewInit(): void {
-    new Promise((resolve, reject) => {
+    const pr = new Promise((resolve, reject) => {
       this.isLoading = false;
+
+      // when we call `resolve`, `then callback` is schedule to be triggered in next(2nd) tick
+      resolve(null); // optional value, `null` is just for type checking safety
+    });
+
+    pr.then(() => {
+      this.isLoading = true;
     });
   }
 }
