ab1b073cba29a741b823cd5f4070d71d51ffc349
Ajit Rana <ajit.thakurs210@gmail.com>
2021-09-24 06:21:35 +0530

    lecture 61 example 6

--- lecture-61/src/app/hello/hello.component.ts
+++ lecture-61/src/app/hello/hello.component.ts
@@ -1,6 +1,4 @@
 import { AfterViewInit, Component } from "@angular/core";
-import { ajax } from "rxjs/ajax";
-import { startWith, tap } from "rxjs/operators";
 
 @Component({
   selector: "app-hello",
@@ -11,24 +9,8 @@ export class HelloComponent implements AfterViewInit {
   isLoading = true;
 
   ngAfterViewInit(): void {
-    ajax
-      .getJSON("https://jsonplaceholder.typicode.com/posts")
-      .pipe(
-        // Solution: comment out or don't use `startWith` operator,
-        // always be careful about the RxJS operators and their behavior
-        // startWith([]),
-        tap(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((list) => {
-        console.log("posts", list);
-      });
+    new Promise((resolve, reject) => {
+      this.isLoading = false;
+    });
   }
 }
-
-// Exercise Problem
-
-// An observable-based exercise problem, try to find the problem and a solution.
-
-// git checkout lecture-61-exercise
