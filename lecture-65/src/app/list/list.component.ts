3d7f293b4f67efe3ddf464994076b5f6d1e43d9a
Ajit Rana <ajit.thakurs210@gmail.com>
2021-10-27 18:56:01 +0530

    lecture 65 d

--- lecture-65/src/app/list/list.component.ts
+++ lecture-65/src/app/list/list.component.ts
@@ -9,52 +9,36 @@ import { ItemComponent, ItemService } from "../item/item.component";
   styleUrls: ["./list.component.css"],
 })
 export class ListComponent implements AfterViewInit {
-  // @1 query using TemplateRef `namedItem` and read `title` values
-  @ViewChildren("namedItem", { read: "title" }) items!: QueryList<string>;
+  // @1 query using string token `id` and `title`, read their string values
+  @ViewChildren("id,title") items!: QueryList<string>;
 
   /********************************/
 
-  // @2 query using string token `id` and read `id` values
-  // read option default to `id`
-  // @ViewChildren("id") items!: QueryList<string>;
+  // @2 query using string token `id` and `title`, read `ItemComponent`
+  // @ViewChildren("id,title", { read: ItemComponent }) items!: QueryList<ItemComponent>;
 
   /********************************/
 
-  // @3 query using string token `title` and read `ItemComponent`
-  // @ViewChildren("title", { read: ItemComponent }) items!: QueryList<ItemComponent>;
+  // @3 query using TemplateRef 'namedItem' and 'item', read `ItemComponent`
+  // in `list.component.html` uncomment the @3,@4 and comment the @1,@2
+  // @ViewChildren("namedItem,item") items!: QueryList<ItemComponent>;
 
   /********************************/
 
-  // @4 query using `ColorDirective` and read `ColorDirective`
-  // read option default to `ColorDirective`
-  // @ViewChildren(ColorDirective) items!: QueryList<ColorDirective>;
-
-  /********************************/
-
-  // @5 query using `ListTypeDirective` and read `ElementRef`
-  // @ViewChildren(ListTypeDirective, { read: ElementRef }) items!: QueryList<ElementRef<HTMLElement>>;
-
-  /********************************/
-
-  // @6 query using `ItemService` and read `ItemService`
-  // read option default to `ItemService`
-  // @ViewChildren(ItemService) items!: QueryList<ItemService>;
-
-  /********************************/
-
-  // @7 query using `ItemService` and read `ListTypeDirective`
-  // @ViewChildren(ItemService, { read: ListTypeDirective }) items!: QueryList<ListTypeDirective>;
+  // @4 query using TemplateRef 'namedItem' and 'item', read string token `id`
+  // in `list.component.html` uncomment the @3,@4 and comment the @1,@2
+  // @ViewChildren("namedItem,item", { read: "id" }) items!: QueryList<string>;
 
   constructor() {}
 
   ngAfterViewInit(): void {
-    // @1 token `title`
+    // @1 token `id` and `title` values
     this.items.forEach((item) => console.log(item));
 
     /********************************/
 
-    // @2 token `id`
-    // this.items.forEach((item) => console.log(item));
+    // @2 `ItemComponent`
+    // this.items.forEach((item) => console.log(item.value));
 
     /********************************/
 
@@ -63,29 +47,7 @@ export class ListComponent implements AfterViewInit {
 
     /********************************/
 
-    // @4 `ColorDirective`
-    // const colors = ["red", "green"];
-    // this.items.forEach((color, index) => color.apply(colors[index]));
-
-    /********************************/
-
-    // @5 `ElementRef`
-    // this.items.forEach((item) => item.nativeElement.classList.add("text-blue"));
-
-    /********************************/
-
-    // @6 `ItemService`
-    // this.items.forEach((item) => item.getItems().subscribe(console.log));
-
-    /********************************/
-
-    // @7 `ListTypeDirective`
-    // watch video `https://youtu.be/4nYlO9TsK60` to know more
-    // const types = [LIST_TYPE.DASH, LIST_TYPE.BULLET];
-    // this.items.forEach((listType, index) => {
-    //   setTimeout(() => {
-    //     listType.apply(types[index]);
-    //   });
-    // });
+    // @4 token`id` values
+    // this.items.forEach((item) => console.log(item));
   }
 }
