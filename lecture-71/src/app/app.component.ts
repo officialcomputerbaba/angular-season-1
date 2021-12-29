c8be6ca0e4d8f2f93e2b039aa1274f66aec5082c
Ajit Rana <ajit.thakurs210@gmail.com>
2021-12-29 23:50:34 +0530

    lecture-71-d

--- lecture-71/src/app/app.component.ts
+++ lecture-71/src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   names = ["Ronaldo", "Ajit", "Sagar", "Anusha", "Aniket", "Pankaj", "Amitabh"];
 
   sortKey: "asc" | "desc" | string = "asc";
+
+  addName(name: string): void {
+    if (!name || !name?.trim().length) {
+      return;
+    }
+
+    this.names.push(name);
+  }
 }
--- lecture-71/src/app/pipes/sort.pipe.ts
+++ lecture-71/src/app/pipes/sort.pipe.ts
@@ -2,6 +2,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
   name: "sort",
+  pure: false,
 })
 export class SortPipe implements PipeTransform {
   transform(list: string[], sortBy: "asc" | "desc" | string = "asc") {
