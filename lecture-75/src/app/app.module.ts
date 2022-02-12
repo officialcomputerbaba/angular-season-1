d9daeda097acb32fc777539e48509f86773279fd
Ajit Rana <ajit.thakurs210@gmail.com>
2022-02-12 22:01:48 +0530

    lecture-75-2

--- lecture-75/src/app/app.module.ts
+++ lecture-75/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
-  declarations: [AppComponent, ClockComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
