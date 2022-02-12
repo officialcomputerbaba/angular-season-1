ae68fecba965695ce4982f60a1dc09d50a4154ac
Ajit Rana <ajit.thakurs210@gmail.com>
2022-02-12 22:01:48 +0530

    lecture-75-2

--- lecture-75/src/app/app.component.ts
+++ lecture-75/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { User } from "./interfaces";
+
+
+function getUsers() {
+  return ajax.getJSON<User[]>("https://jsonplaceholder.typicode.com/users");
+}
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  isShowing = true;
+export class AppComponent implements OnInit {
+  usersList$!: Observable<User[]>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.usersList$ = getUsers();
+  }
 }
