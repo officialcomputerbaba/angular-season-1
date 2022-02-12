bff00eb78ae4485dccb6d427fbfbd63b1a09ab83
Ajit Rana <ajit.thakurs210@gmail.com>
2022-02-12 22:04:27 +0530

    lecture-75-4

--- lecture-75/src/app/app.module.ts
+++ lecture-75/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { PostComponent } from "./post/post.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
