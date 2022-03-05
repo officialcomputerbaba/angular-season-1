efdb4d9b2eb93f61b9450ea5fd637ec0eb30265d
Ajit Rana <ajit.thakurs210@gmail.com>
2022-03-05 16:08:34 +0530

    lecture-78-3

--- lecture-78/src/app/app.module.ts
+++ lecture-78/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { ListComponent } from "./list/list.component";
 import { ListDirective } from "./list.directive";
 
 @NgModule({
-  declarations: [AppComponent, ListComponent, ListDirective],
+  declarations: [AppComponent, ListDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- lecture-78/src/app/list.directive.ts
+++ lecture-78/src/app/list.directive.ts
@@ -1,31 +1,19 @@
-import { Directive, ElementRef, ViewContainerRef } from "@angular/core";
-import { ListComponent } from "./list/list.component";
+import { Directive, ElementRef, TemplateRef } from "@angular/core";
 
 @Directive({
   selector: "[list]",
 })
 export class ListDirective {
-  constructor(
-    private readonly elemRef: ElementRef,
-    private readonly cmp: ListComponent,
-    private readonly container: ViewContainerRef
-  ) {
+  constructor(private readonly elemRef: ElementRef, private readonly templateRef: TemplateRef<any>) {
     this.listOperation();
   }
 
   private listOperation() {
-    // component is also an element therefore we can get `ElementRef` as well
+    // `ng-template` is also an element rendered as comment in DOM
+    // therefore we can get `ElementRef` as well
     console.log(this.elemRef.nativeElement);
 
-    // view container reference of `ListComponent`
-    console.log(this.container);
-
-    // `ListComponent`
-    console.log(this.cmp);
-
-    // component operations
-    this.cmp.focus();
-    this.cmp.selected = true;
-    this.cmp.value = "My List";
+    // `<ng-template>` reference
+    console.log(this.templateRef);
   }
 }
