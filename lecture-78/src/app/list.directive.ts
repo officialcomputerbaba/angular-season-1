65dadbdac08b8e7e2436e52601248d3a41389642
Ajit Rana <ajit.thakurs210@gmail.com>
2022-03-05 16:05:49 +0530

    lecture-78-5

--- lecture-78/src/app/list.directive.ts
+++ lecture-78/src/app/list.directive.ts
@@ -1,17 +1,27 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, ViewContainerRef } from "@angular/core";
 import { ListComponent } from "./list/list.component";
 
 @Directive({
   selector: "[list]",
 })
 export class ListDirective {
-  constructor(private readonly elemRef: ElementRef, private readonly cmp: ListComponent) {
+  constructor(
+    private readonly elemRef: ElementRef,
+    private readonly cmp: ListComponent,
+    private readonly container: ViewContainerRef
+  ) {
     this.listOperation();
   }
 
   private listOperation() {
     // component is also an element therefore we can get `ElementRef` as well
     console.log(this.elemRef.nativeElement);
+
+    // view container reference of `ListComponent`
+    console.log(this.container);
+
+    // `ListComponent`
+    console.log(this.cmp);
 
     // component operations
     this.cmp.focus();
