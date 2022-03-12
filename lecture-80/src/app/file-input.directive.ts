00408a1da93f5e5fb05370c14221f736bb173a35
Ajit Rana <ajit.thakurs210@gmail.com>
2022-03-13 04:31:12 +0530

    lecture-80-2

--- lecture-80/src/app/file-input.directive.ts
+++ lecture-80/src/app/file-input.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef, OnInit, OnChanges, SimpleChanges, Input } from "@angular/core";
+import { Directive, ElementRef, OnInit, Input } from "@angular/core";
 
 @Directive({
   selector: "[fileInput]",
 })
-export class FileInputDirective implements OnInit, OnChanges {
+export class FileInputDirective implements OnInit {
   private inputRef!: HTMLInputElement;
 
   private mime!: string;
@@ -17,7 +17,14 @@ export class FileInputDirective implements OnInit, OnChanges {
   isImage = false;
 
   @Input()
-  fileInput!: [string, number] | string;
+  set fileInput(mimeOrTuple: [string, number] | string) {
+    if (Array.isArray(mimeOrTuple) && mimeOrTuple.length === 2) {
+      this.mime = mimeOrTuple[0];
+      this.allowedSize = mimeOrTuple[1];
+    } else if (typeof mimeOrTuple === "string" && mimeOrTuple?.trim()?.length) {
+      this.mime = mimeOrTuple;
+    }
+  }
 
   constructor(private readonly elemRef: ElementRef) {
     this.inputRef = this.elemRef.nativeElement;
@@ -25,19 +32,6 @@ export class FileInputDirective implements OnInit, OnChanges {
 
   ngOnInit() {
     this.inputRef.addEventListener("change", () => this.onFileInputChange());
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    const fileInput = changes["fileInput"];
-
-    const mimeOrTuple = fileInput.currentValue;
-
-    if (Array.isArray(mimeOrTuple) && mimeOrTuple.length === 2) {
-      this.mime = mimeOrTuple[0];
-      this.allowedSize = mimeOrTuple[1];
-    } else if (typeof mimeOrTuple === "string" && mimeOrTuple?.trim()?.length) {
-      this.mime = mimeOrTuple;
-    }
   }
 
   private async onFileInputChange() {
