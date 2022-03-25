639a8c5dd9267c911fd61bda9cea08d7e57b737c
Ajit Rana <ajit.thakurs210@gmail.com>
2022-03-25 19:55:08 +0530

    lecture-87-2

--- lecture-87/src/app/paint.directive.ts
+++ lecture-87/src/app/paint.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, ElementRef, TemplateRef } from "@angular/core";
+import { Directive, ElementRef, TemplateRef, ViewContainerRef } from "@angular/core";
 
 @Directive({
   selector: "[paint]",
 })
 export class PaintDirective {
-  constructor(private readonly elemRef: ElementRef, private readonly template: TemplateRef<any>) {
+  constructor(
+    private readonly elemRef: ElementRef,
+    private readonly template: TemplateRef<any>,
+    private readonly container: ViewContainerRef
+  ) {
     // `ElementRef` - render as comment in DOM
     // `TemplateRef` - associated template
+    // `ViewContainerRef` - associated view container
     console.log(this.elemRef.nativeElement, this.template);
+  }
+
+  ngOnInit() {
+    // clear the previous view, if any
+    this.container.clear();
+
+    // create the view from this `template`
+    this.container.createEmbeddedView(this.template);
   }
 }
